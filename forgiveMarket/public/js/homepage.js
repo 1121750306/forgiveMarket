98c1777f9a790a75863dd606020b2a476da5380f
zxw <pc666@pc666-PC>
2017-07-13 15:48:20 +0800

    homepage 更新到hotgoods

--- forgiveMarket/public/js/homepage.js
+++ forgiveMarket/public/js/homepage.js
@@ -1,6 +1,61 @@
-/*------------------------------------------头部*/
-/*--------------------------------------------搜索栏*/
-/*------------------------------------------广告轮播图*/
-/*------------------------------------------新闻轮播图*/
-/*------------------------------------------选项卡*/
-$(".")
+$(function(){
+	/*------------------------------------------头部*/
+	/*--------------------------------------------搜索栏*/
+	/*------------------------------------------广告轮播*/
+	var i = 0;
+	var j = 0;
+	var _left;
+	function showImg()
+	{
+		i = i+1;
+		_left = i*(-405)+"px";
+		$(".adcontainer").animate({left: _left},1500,function(){
+			if(parseInt($(".adcontainer").css("left")) < -2025)
+			{
+				$(".adcontainer").css("left","0px");
+				i = 0;
+			}
+		});
+	}
+	setInterval(showImg,3000);
+	function showList()
+	{
+		j = j+1;
+		if(j > 6)
+		{
+			j =0;
+		}		
+		$(".adturnullist").css("background-color","#CED4D2")
+		$("#l"+(j+1)).css("background-color","orange")
+	}
+	setInterval(showList,3000);
+	/*------------------------------------------新闻轮播*/
+	
+	function shownews()
+	{
+		$(".newscontainer").animate({left: '-1000px'},30000,
+		function(){
+			if(parseInt($(this).css("left"))< -800)
+			{
+				$(this).css("left","400px");
+			}
+		})		
+	}
+	shownews();
+	setInterval(shownews,30000);
+	
+	/*------------------------------------------选项卡*/
+	$(".lnbtitle-info").click(function(){
+		$(".innisinformation").css("display","inline-block");
+		$(".activities").css("display","none");
+		$(".communicate").css("display","none");
+	});
+	$(".lnbtitle-act").click(function(){
+		$(".innisinformation").css("display","none");
+		$(".activities").css("display","inline-block");
+		$(".communicate").css("display","none");
+	});
+	
+	
+});
+
