15ab1e6ab814c994a816a78036fb503377f39701
carl <271372080@qq.com>
2017-07-14 13:26:54 +0800

    无意义

--- forgiveMarket/public/js/navigation.js
+++ forgiveMarket/public/js/navigation.js
@@ -1,16 +1,16 @@
-$(function(){
+$(function() {
 	//打开侧滑菜单
-	$(".nav_menu").click(function(){
-		
+	$(".nav_menu").click(function() {
+
 	})
-	
+
 	//打开个人空间
-	$(".nav_user").click(function(){
-		
+	$(".nav_user").click(function() {
+		self.location = "/views/user/user.html";
 	})
-	
+
 	//打开购物车
-	$(".nav_cart").click(function(){
+	$(".nav_cart").click(function() {
 		self.location = "/order";
 	})
-})
+});
