881dc08222c0ad306a363ade804a397af7df8c76
zxw <pc666@pc666-PC>
2017-07-14 14:11:37 +0800

    homepage 未搞事情部分全部完成 接下来进行搞事情部分

--- forgiveMarket/public/js/homepage.js
+++ forgiveMarket/public/js/homepage.js
@@ -8,9 +8,9 @@ $(function(){
 	function showImg()
 	{
 		i = i+1;
-		_left = i*(-410)+"px";
+		_left = i*(-412)+"px";
 		$(".adcontainer").animate({left: _left},1500,function(){
-			if(parseInt($(".adcontainer").css("left")) < -2025)
+			if(parseInt($(".adcontainer").css("left")) < -2060)
 			{
 				$(".adcontainer").css("left","0px");
 				i = 0;
@@ -64,6 +64,95 @@ $(function(){
 		$(this).css("color","#799656");
 		$(this).css("border-bottom","2px solid #799656")
 	})
+	
+	/*-----------------------------------------------搞事情的叶子*/
+	window.onscroll = function()
+	{
+		var scrollh = document.body.scrollTop ||  document.documentElement.scrollTop;
+		console.log(scrollh)
+		if(scrollh >0 && scrollh < 741)
+		{
+			$(".yezi").css("display","inline-block")
+			$(".yezi").css("margin-top","640px")
+		}
+		else if(scrollh>741 && scrollh <800)
+		{
+			$(".yezi").css("margin-top","700px")
+			$(".yezi").css("margin-left","45%")
+		}
+		
+		else if (scrollh >800 && scrollh < 900)
+		{
+			$(".yezi").css("margin-top","780px")
+			$(".yezi").css("margin-left","45%")
+		}
+		else if (scrollh >900 && scrollh < 1000)
+		{
+			$(".yezi").css("margin-top","820px")
+			$(".yezi").css("margin-left","45%")
+		}
+		else if (scrollh >1000 && scrollh < 1100)
+		{
+			$(".yezi").css("margin-top","880px")
+			$(".yezi").css("margin-left","45%")
+		}
+		else if (scrollh >1100 && scrollh < 1200)
+		{
+			$(".yezi").css("margin-top","970px")
+			$(".yezi").css("margin-left","18%")
+		}
+		else if (scrollh >1200 && scrollh < 1300)
+		{
+			$(".yezi").css("margin-top","1070px")
+			$(".yezi").css("margin-left","55%")
+		}
+		else if (scrollh >1300 && scrollh < 1400)
+		{
+			$(".yezi").css("margin-top","1130px")
+			$(".yezi").css("margin-left","53%")
+		}
+		else if (scrollh >1400 && scrollh < 1450)
+		{
+			$(".yezi").css("margin-top","1200px")
+			$(".yezi").css("margin-left","83%")
+		}
+		else if (scrollh >1450 && scrollh < 1500)
+		{
+			$(".yezi").css("margin-top","1250px")
+			$(".yezi").css("margin-left","25%")
+		}
+		else if (scrollh >1500 && scrollh < 1550)
+		{
+			$(".yezi").css("margin-top","1280px")
+			$(".yezi").css("margin-left","25%")
+		}
+		else if (scrollh >1550 && scrollh < 1600)
+		{
+			$(".yezi").css("margin-top","1330px")
+			$(".yezi").css("margin-left","30%")
+			$(".yezi").animate({
+				left : '60%'
+			},1000)
+		}
+		
+		/*--------------------------popdiv*/
+		else if (scrollh > 1600 && scrollh <1650)
+		{
+			$(".popdiv").slideDown();
+			window.onmousewheel = function()
+			{
+				return false;
+			}
+			$(".popdiv-close").click(function(){
+				$(".popdiv").slideUp();
+				window.onmousewheel = function()
+				{
+					return true;
+				}
+			})
+		}
+	
+	}
 	
 });
 
