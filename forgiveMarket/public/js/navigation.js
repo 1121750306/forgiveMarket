1326d39c6ea77dbbcced46fc6e875767d8b6d372
czRadiance <czRadiance@icloud.com>
2017-07-15 20:51:22 +0800

    商品类别页面 前端页面修改

--- forgiveMarket/public/js/navigation.js
+++ forgiveMarket/public/js/navigation.js
@@ -1,8 +1,44 @@
 $(function() {
 	//打开侧滑菜单
 	$(".nav_menu").click(function() {
+		$(".nav").parent().css("overflow","hidden");
 		$(".nav_modal").show();
 		$(".nav_modal_content").animate({"right":"20%"},500);
+		
+		
+		var goodtypes = JSON.parse(sessionStorage.getItem("goodtypes"));
+		
+		if (goodtypes == null) {
+			//请求商品类型数据
+			$.ajax({
+				type:"get",
+				url:"/goodtype",
+				async:true,
+				success:function(data){
+					if (data != "error") {
+						//session中存储商品类型
+						sessionStorage.setItem("goodtypes", JSON.stringify(data));
+						goodtypes = data;
+						
+						$(".nav_modal_type").empty();
+						for (var i = 0; i < goodtypes.length; i++) {
+							$(".nav_modal_type").append("<li><a href='/good:" +goodtypes[i]._id+ "'>" + goodtypes[i].tname + "</a></li>");
+						}
+					}
+				},
+				error:function(err){
+					console.log("ERROR: get all good types fail");
+					console.log("ERROR CONTENT: " + err);
+				}
+			});
+		}else{
+			
+			$(".nav_modal_type").empty();
+			for (var i = 0; i < goodtypes.length; i++) {
+				$(".nav_modal_type").append("<li><a href='/good:" +goodtypes[i]._id+ "'>" + goodtypes[i].tname + "</a></li>");
+			}
+		}
+		
 	});
 	
 	//关闭侧滑菜单
@@ -10,12 +46,18 @@ $(function() {
         var tar = $(this).children(".nav_modal_content");
 
         if (!tar.is(e.target) && (tar.has(e.target).length === 0)){
-        	$(".nav_modal_content").animate({"right":"100%"},500,function(){$(".nav_modal").hide()});
+        	$(".nav_modal_content").animate({"right":"100%"},500,function(){
+        		$(".nav_modal").hide();
+        		$(".nav").parent().css("overflow","auto");
+    		});
         }
 
     });
     $(".nav_modal_close").click(function(){
-    	$(".nav_modal_content").animate({"right":"100%"},500,function(){$(".nav_modal").hide()});
+    	$(".nav_modal_content").animate({"right":"100%"},500,function(){
+    		$(".nav_modal").hide();
+			$(".nav").parent().css("overflow","auto");
+    	});
     });
     
     //切换侧滑菜单页
