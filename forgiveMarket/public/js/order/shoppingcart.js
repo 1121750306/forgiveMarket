6f232c728082ddbcd1714f3891e4bfd22f980390
czRadiance <czRadiance@icloud.com>
2017-07-16 15:40:08 +0800

    购物车界面 静态js基本完成

--- forgiveMarket/public/js/order/shoppingcart.js
+++ forgiveMarket/public/js/order/shoppingcart.js
@@ -34,6 +34,9 @@ $(function(){
 			//多选框未确认
 			$(this).parent().css("background-image","url(../../img/innisfreeIco/checked.png)");
 			
+			//删除按钮显示
+			$(".check_delete").show();
+			
 			//当所有多选框确认时 全选框同时确认
 			$(".item_allcheck").css("background-image","url(../../img/innisfreeIco/checked.png)");
 			for (var i = 0; i < itemChecks.length - 1; i++) {
@@ -44,7 +47,7 @@ $(function(){
 			}
 			
 			if ($(this).parent()[0] != $(".item_allcheck")[0]) {
-				//商品总价增加当前确认的商品
+				//商品总价增加当前勾选的商品
 				var price = parseFloat($(this).parent().parent().find(".item_price").html().substr(1));
 				var num = parseFloat($(this).parent().parent().find(".item_num").val());
 				var totalprice = parseFloat($(".totalprice").html().substr(4)) + price*num;
@@ -53,7 +56,7 @@ $(function(){
 				
 				//结算商品数量增加
 				var totalnum = $(".goodscheck").html().substr(4);
-				totalnum = parseInt(totalnum.substring(0,totalnum.length - 1)) + 1;
+				totalnum = parseInt(totalnum.substring(0,totalnum.length - 1)) + num;
 				$(".goodscheck").html("去结算(" + totalnum + ")");
 				
 			}
@@ -63,6 +66,16 @@ $(function(){
 			//多选框已确认
 			$(this).parent().css("background-image","url(../../img/innisfreeIco/unchecked.png)");
 			
+			//当有一个多选框确认时 删除按钮显示
+			$(".check_delete").hide();
+			for (var i = 0; i < itemChecks.length - 1; i++) {
+				if (itemChecks.eq(i)[0].checked && itemChecks.eq(i)[0] != $(this).parent()[0]) {
+					console.log(i);
+					$(".check_delete").show();
+					break;
+				}
+			}
+			
 			//当一个多选框取消时 全选框同时取消
 			if ($(this).parent()[0] != $(".item_allcheck")[0]) {
 				$(".item_allcheck").css("background-image","url(../../img/innisfreeIco/unchecked.png)");
@@ -70,7 +83,7 @@ $(function(){
 			}
 			
 			if ($(this).parent()[0] != $(".item_allcheck")[0]) {
-				//商品总价减少当前确认的商品
+				//商品总价减少当前勾选的商品
 				var price = parseFloat($(this).parent().parent().find(".item_price").html().substr(1));
 				var num = parseFloat($(this).parent().parent().find(".item_num").val());
 				var totalprice = parseFloat($(".totalprice").html().substr(4)) - price*num;
@@ -79,7 +92,7 @@ $(function(){
 				
 				//结算商品数量减少
 				var totalnum = $(".goodscheck").html().substr(4);
-				totalnum = parseInt(totalnum.substring(0,totalnum.length - 1)) - 1;
+				totalnum = parseInt(totalnum.substring(0,totalnum.length - 1)) - num;
 				$(".goodscheck").html("去结算(" + totalnum + ")");
 				
 			}
@@ -92,6 +105,13 @@ $(function(){
 		var allChecked = true;
 		var checkBtn = $(".item_check");
 		
+		//购物车为空
+		if (checkBtn.length <= 1) {
+			$(".item_allcheck").css("background-image","url(../../img/innisfreeIco/unchecked.png)");
+			$(".item_allcheck input")[0].checked = false;
+		}
+		
+		//判断是否已经全选
 		for (var i = 0; i < checkBtn.length - 1; i++) {
 			if (!checkBtn.eq(i).children("input")[0].checked) {
 				allChecked = false;
@@ -109,6 +129,7 @@ $(function(){
 				$(".totalprice").html("总价￥:0.00");
 				$(".goodscheck").html("去结算(0)");
 			}
+			
 		}else{
 			//未全选
 			for (var i = 0; i < checkBtn.length - 1; i++) {
@@ -116,7 +137,7 @@ $(function(){
 					checkBtn.eq(i).css("background-image","url(../../img/innisfreeIco/checked.png)");
 					checkBtn.eq(i).children("input")[0].checked = true;
 										
-					//商品总价增加当前确认的商品
+					//商品总价增加当前勾选的商品
 					var price = parseFloat(checkBtn.eq(i).parent().find(".item_price").html().substr(1));
 					var num = parseFloat(checkBtn.eq(i).parent().find(".item_num").val());
 					var totalprice = parseFloat($(".totalprice").html().substr(4)) + price*num;
@@ -126,7 +147,7 @@ $(function(){
 					//结算商品数量增加
 					var totalnum = $(".goodscheck").html().substr(4);
 					totalnum = parseInt(totalnum.substring(0,totalnum.
-						length - 1)) + 1;
+						length - 1)) + num;
 					$(".goodscheck").html("去结算(" + totalnum + ")");
 				}
 			}
@@ -136,12 +157,53 @@ $(function(){
 
 	//商品数量控制
 	$(".item_add").click(function(){
-		var num = $(this).parent().parent().children("input[type=text]");
-		num.val((parseInt(num.val())+1)>99?99:parseInt(num.val())+1);
+		var item = $(this).parents(".cart_item");
+		var num = parseInt(item.find(".item_num").val());
+		
+		//商品数量小于99时可增加
+		if (num < 99) {
+			item.find(".item_num").val(num + 1);
+			
+			//如果商品已被勾选
+			if (item.find(".item_check input")[0].checked) {
+				//商品总价增加一个当前勾选的商品
+				var price = parseFloat(item.find(".item_price").html().substr(1));
+				var totalprice = parseFloat($(".totalprice").html().substr(4)) + price;
+				totalprice = totalprice.toFixed(2);
+				$(".totalprice").html("总价￥:" + totalprice);
+			
+				//结算商品数量增加
+				var totalnum = $(".goodscheck").html().substr(4);
+				totalnum = parseInt(totalnum.substring(0,totalnum.
+					length - 1)) + 1;
+				$(".goodscheck").html("去结算(" + totalnum + ")");
+			}
+		}
 	});
 	$(".item_sub").click(function(){
-		var num = $(this).parent().parent().children("input[type=text]");
-		num.val((parseInt(num.val())-1)<1?1:parseInt(num.val())-1);
+		var item = $(this).parents(".cart_item");
+		var num = parseInt(item.find(".item_num").val());
+		
+		//商品数量大于1时可减少
+		if (num > 1) {
+			item.find(".item_num").val(num - 1);
+			
+			//如果商品已被勾选
+			if (item.find(".item_check input")[0].checked) {
+				//商品总价减少一个当前勾选的商品
+				var price = parseFloat(item.find(".item_price").html().substr(1));
+				var totalprice = parseFloat($(".totalprice").html().substr(4)) - price;
+				totalprice = totalprice.toFixed(2);
+				$(".totalprice").html("总价￥:" + totalprice);
+			
+				//结算商品数量减少
+				var totalnum = $(".goodscheck").html().substr(4);
+				totalnum = parseInt(totalnum.substring(0,totalnum.
+					length - 1)) - 1;
+				$(".goodscheck").html("去结算(" + totalnum + ")");
+			}
+		}
+				
 	});
 	$(".info_bottom").children("input[type=text]").keydown(function(){
 		if (event.keyCode != 8 && event.keyCode != 37 && event.keyCode != 39 && (event.keyCode > 57 || event.keyCode < 48)) {
@@ -160,6 +222,30 @@ $(function(){
 		if ($(this).val() == "") {
 			$(this).val(1);
 		}
+	});
+	
+	//删除选中控制
+	$(".check_delete").click(function(){
+		var item = $(".cart_item");
+		
+		//遍历所有被勾选的商品
+		for (var i = 0; i < item.length; i++) {
+			if (item.eq(i).find(".item_check input")[0].checked) {
+				item.eq(i).remove();
+			}
+		}
+		
+		//全选框取消确认
+		$(".item_allcheck").css("background-image","url(../../img/innisfreeIco/unchecked.png)");
+		$(".item_allcheck input")[0].checked = false;
+							
+		//商品总价和数量归0
+		$(".totalprice").html("总价￥:0.00");
+		$(".goodscheck").html("去结算(0)");
+		
+		//删除按钮消失
+		$(this).hide();
+		
 	});
 	
 })
