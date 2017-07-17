04777366f6e82d94acdeee6989ee3230452745d9
czRadiance <czRadiance@icloud.com>
2017-07-17 11:52:26 +0800

    购物车数据获取方式改变

--- forgiveMarket/models/order.js
+++ forgiveMarket/models/order.js
@@ -4,14 +4,53 @@ function initModel(models) {
     orderModel =  models.order;
 }
 
+
+function addOrder (uid, flag, callback) {
+	switch (flag){
+		//购物车
+		case 0:
+			var orderEntity = new orderModel({
+				uid: uid,
+				flag: 0,
+				date: new Date()
+			});
+			
+			orderEntity.save(callback);
+			break;
+		//未付款
+		case 1:
+			break;
+		//已付款
+		case 2:
+			break;
+		//已发货
+		case 3:
+			break;
+		//已收货
+		case 4:
+			break;
+		//已收货
+		case 5:
+			break;
+		default:
+			break;
+	}
+}
+
 /**
- * 添加订单
+ * 查找订单
  * @param {Object} uid
  * @param {Object} flag
+ * @param {Object} callback
  */
-function addOrder (uid, flag) {
-	
+function getOrder (uid, flag, callback) {
+	orderModel.find({
+		uid: uid,
+		flag: flag
+	},callback);
 }
 
 
 module.exports.initModel = initModel;
+module.exports.addOrder = addOrder;
+module.exports.getOrder = getOrder;
