eb5ff588696088ef348673195fade26a2d67b07c
wmf <42696174@qq.com>
2017-07-18 13:50:31 +0800

    上传图片

--- forgiveMarket/routes/admin.js
+++ forgiveMarket/routes/admin.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var user = require("../models/user");
+var goodphoto = require("../models/goodphoto");
 var multiparty = require('multiparty');
-
+var fs=require('fs');
 //注册models监听并传递models
 function callback(models) {
 	user.initModel(models);
@@ -28,7 +29,32 @@ router.post("/upload",function(req,res,next){
 		uploadDir: './public/img/upload'
 	});
 	form.parse(req,function(err, fields, files){
-		console.log('files:' + files);
+		var goodid=fields.goodid;
+		console.log("goodid"+goodid);
+		var inputFile = files.photo != undefined ? files.photo[0] : null;
+		var uploadedPath = inputFile != null ? inputFile.path : null;
+		var types=uploadedPath.split(".")[1];
+		var type;
+		if(types.toLowerCase()=="jpg"){
+			type=
+		}
+		console.log('inputFile:' + inputFile);
+		console.log('uploadedPath:' + uploadedPath);
+		if(uploadedPath != null) {
+			uploadedPath = uploadedPath.substr(uploadedPath.indexOf("\\"), uploadedPath.length);
+		};
+		fs.rename(files.photo[0].path,'./public/img/upload/'+files.photo[0].originalFilename,function(err){
+			/*console.log(err);*/
+			if(!err){
+				console.log(files.photo[0].originalFilename);
+				var urls=files.photo[0].originalFilename;
+				res.send({msg:"success"});
+			}else{
+				res.send({msg:"fail"});
+				console.log(err);
+			}
+		});
+	    
 		
 	})
 })
