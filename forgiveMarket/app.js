37c83a617df9f05e08913f4f114654148bb82aa6
carl <271372080@qq.com>
2017-07-18 14:31:27 +0800

    修改app

--- forgiveMarket/app.js
+++ forgiveMarket/app.js
@@ -56,6 +56,7 @@ app.use('/order', order);
 app.use('/location',locations);
 app.use('/admin',admin);
 app.use('/history',history);
+app.use('/collect',collect);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	var err = new Error('Not Found');
