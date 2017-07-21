0fca0435b7a1d2119918c7a125d033b293c95322
czRadiance <czRadiance@icloud.com>
2017-07-21 14:52:00 +0800

    订单界面 确认收货完成

--- forgiveMarket/public/js/order/orderlist.js
+++ forgiveMarket/public/js/order/orderlist.js
@@ -13,17 +13,7 @@ $(function() {
 		}
 	});
 	
-	//查看收货信息控制
-	$(".order_location").click(function(){
-		$(".order").empty();
-		reloadView(view);
-	});
-	
 	//商品评论控制
-	$(".item_comment").click(function(){
-		$(".com_txt form input[name=gid]").val($(this).parents(".cart_item").attr("gid"));
-		$(".comment").animate({top:"0%"},500);
-	});
 	$(".com_cancel").click(function(){
 		$(".comment").animate({top:"100%"},500);
 	})
@@ -35,23 +25,6 @@ $(function() {
 		
 		//修改订单页类型
 		$(".order_type li").eq(index).addClass("underline").siblings("li").removeClass("underline");
-		
-		//当在已收货界面时显示评价按钮
-		$(".item_comment").remove();
-		if (index == 3) {
-			$(".info_bottom").append("<span class='item_comment'>评价</span>");
-			$(".item_comment").bind("click",tocomment);
-		}
-		
-		//当在未付款界面时显示去付款按钮,在已发货界面时显示确认收货按钮
-		$(".order_operation").remove();
-		if (index == 0) {
-			$(".order_location").after("<span class='order_operation'>去付款</span>");
-			$(".order_operation").bind("click",topay);
-		}else if (index == 2){
-			$(".order_location").after("<span class='order_operation'>确认收货</span>");
-			$(".order_operation").bind("click",tofinish);
-		}
 		
 		//清空order
 		$(".order").empty();
@@ -70,25 +43,57 @@ $(function() {
 						let order = data.result[i];
 						console.log(order);
 						
+						let location = order.locationid.province + order.locationid.city + 
+										order.locationid.district + order.locationid.address;
+						
 						//组合订单li
-						let li ='<li class="order_item" oid="' + order.gid + '">\
+						let li ='<li class="order_item" oid="' + order.oid + '">\
 									<div class="order_top">\
 										<p class="order_date">订单时间：' + order.date + '</p>\
 									</div>\
 									<ul class="cart"></ul>\
+									<ul class="location_info">\
+										<li>\
+											<h4>收货人：</h4>\
+											<h4 class="location_name">' + order.locationid.shname + '</h4>\
+										</li>\
+										<li>\
+											<h4>手机号：</h4>\
+											<h4 class="location_phone">' + order.locationid.phone + '</h4>\
+										</li>\
+										<li>\
+											<h4>收货地址：</h4>\
+											<h4 class="location_address">' + location + '</h4>\
+										</li>\
+									</ul>\
 									<div class="order_bottom">\
 										<span class="order_location" >查看收货信息</span>\
 										<p class="order_price">总计：￥' + order.total.toFixed(2) + '</p>\
 									</div>\
 								</li>';
-						
+												
 						$(".order").append(li);
+						
+						//查看收货信息绑定事件
+						$(".order .order_item").eq(i).find(".order_location").bind("click",tolocation);
+																					
+						//当在未付款界面时显示去付款按钮,在已发货界面时显示确认收货按钮
+						if (index == 0) {
+							$(".order .order_item").eq(i).find(".order_location")
+								.after("<span class='order_operation'>去付款</span>");
+							//绑定事件
+							$(".order .order_item").eq(i).find(".order_operation").bind("click",topay);
+						}else if (index == 2){
+							$(".order .order_item").eq(i).find(".order_location")
+								.after("<span class='order_operation'>确认收货</span>");
+							//绑定事件
+							$(".order .order_item").eq(i).find(".order_operation").bind("click",tofinish);
+						}		
 						
 						//获得所有订单项
 						let orderitems = order.orderitem;
 						
-						//拼接订单项li
-						let itemLi = "";
+						//遍历订单项li
 						for (let j = 0; j < orderitems.length; j++) {
 							let orderitem = orderitems[j];
 							console.log(orderitem);
@@ -98,7 +103,8 @@ $(function() {
 								url:"/goodphoto/getShowPhoto/" + orderitem.gid,
 								async:true,
 								success:function(photo){
-									itemLi +='<li class="cart_item" gid= "' + orderitem.gid + '">\
+									//拼接订单项li
+									let itemLi ='<li class="cart_item" gid= "' + orderitem.gid + '">\
 												<img src="/img/upload/' + photo + '"/>\
 												<div class="item_info">\
 													<h2>' + orderitem.gname + '</h2>\
@@ -111,7 +117,15 @@ $(function() {
 											</li>';
 											
 									$(".order .order_item").eq(i).children(".cart").append(itemLi);
-											
+									
+									//当在已收货界面时显示评价按钮
+									if (index == 3) {
+										$(".order .order_item").eq(i).find(".cart .cart_item").eq(j)
+											.find(".info_bottom").append("<span class='item_comment'>评价</span>");
+										$(".order .order_item").eq(i).find(".cart .cart_item").eq(j)
+											.find(".item_comment").bind("click",tocomment);
+									}
+									
 								},
 								error:function(err){
 									console.log(err);
@@ -135,19 +149,61 @@ $(function() {
 
 	}
 	
+	//查看收货信息控制
+	function tolocation (e) {
+		$(e.target).html($(e.target).html() == "查看收货信息"?"收起收货信息":"查看收货信息")
+					.parent().siblings(".location_info").slideToggle();
+		$(e.target).parents(".order_item").siblings("li").find(".location_info").slideUp();
+		$(e.target).parents(".order_item").siblings("li").find(".order_location").html("查看收货信息");
+	}
+	
 	//去付款控制
 	function topay () {
 		alert("pay")
 	}
 	
 	//确认收货控制
-	function tofinish () {
-		alert("finish")
+	function tofinish (e) {
+		var oid = $(e.target).parents(".order_item").attr("oid");
+		
+		$.ajax({
+			type:"post",
+			url:"/order/updateorder",
+			data:{
+				otid:oid,
+				flag:4,
+			},
+			async:true,
+			success:function(data){
+				reloadView(2);
+			},
+			error:function(err){
+				console.log(err);
+			}
+		});
 	}
 	
 	//评论控制
-	function tocomment () {
-		alert("comment")
+	function tocomment (e) {
+		$(".com_txt form input[name=gid]").val($(this).parents(".cart_item").attr("gid"));
+		$(".comment").animate({top:"0%"},500);
+//		var oid = $(e.target).parents(".order_item").attr("oid");
+//		
+//		$.ajax({
+//			type:"post",
+//			url:"/order/updateorder",
+//			data:{
+//				otid:oid,
+//				flag:3,
+//			},
+//			async:true,
+//			success:function(data){
+//				reloadView(2);
+//			},
+//			error:function(err){
+//				console.log(err);
+//			}
+//		});
 	}
 	
 });
