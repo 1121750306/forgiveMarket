67120b82e1a027579a2b6c226441cd48a4249294
Dark996 <271372080@qq.com>
2017-07-22 14:45:30 +0800

    个人首页订单数量补充

--- forgiveMarket/routes/goodtype.js
+++ forgiveMarket/routes/goodtype.js
@@ -9,7 +9,7 @@ function callback(models) {
 
 //获得商品类型列表
 router.get('/', function(req, res, next) {
-	 goodtype.addGoodTypes(["面部护理","身体护理","隔离防晒","妆前打底","修颜遮瑕","面部彩妆","清洁卸妆","男士系列","指甲油","美容工具","优惠套装","会员专区"]);
+//	 goodtype.addGoodTypes(["面部护理","身体护理","隔离防晒","妆前打底","修颜遮瑕","面部彩妆","清洁卸妆","男士系列","指甲油","美容工具","优惠套装","会员专区"]);
 	console.log("goodtype router get");
 	goodtype.getGoodTypes(function(err, data){
 		if (!err) {
