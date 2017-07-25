a1d55ce7f26abc69bef0f5f82ff3e163161ba35e
wmf <42696174@qq.com>
2017-07-25 21:37:30 +0800

    gai duankou

--- forgiveMarket/routes/index.js
+++ forgiveMarket/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	console.log("2222222222222");
+  //res.render('index', { title: 'Express' });
+  res.redirect("/views/homepage.html");
 });
 
 module.exports = router;
